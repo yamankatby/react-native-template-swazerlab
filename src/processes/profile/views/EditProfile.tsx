431438ddeabb35917cbabb7514a532b63c9f01b6
Yaman KATBY <m.yaman.katby@gmail.com>
2020-02-27 14:48:49 +0300

    🚧 Work on Profile screen

--- src/processes/profile/views/EditProfile.tsx
+++ src/processes/profile/views/EditProfile.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { useScreenTracker } from '../../../config/firebase';
 
 const EditProfile = () => {
 	useScreenTracker('EditProfile');
 	return (
 		<View>
-			<Text>Hi, EditProfile Component!</Text>
+
 		</View>
 	);
 };
--- src/processes/profile/views/Profile.tsx
+++ src/processes/profile/views/Profile.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import Table, { Section, StaticCell } from 'react-native-js-tableview';
+import { View } from 'react-native';
+import Table, { BioCell, KeyValueCell, Section, StaticCell } from 'react-native-js-tableview';
 import { useScreenTracker } from '../../../config/firebase';
-import { useDispatch } from '../../../config/store';
+import { useDispatch, useSelector } from '../../../config/store';
 import { logOut } from '../../auth/logic/actions';
+import { navigate } from '../../services/logic/actions';
 
 const Profile = () => {
 	useScreenTracker('Profile');
 	const dispatch = useDispatch();
+	const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+	const name = useSelector(state => state.profile.profile?.name);
+	const email = useSelector(state => state.profile.profile?.email);
+	const phoneNumber = useSelector(state => state.profile.profile?.phoneNumber);
+
+	if (!isLoggedIn) return <View />;
 	return (
 		<Table>
+			<Section>
+				<BioCell
+					title={name!}
+					subtitle={email}
+					accessory={'disclosure'}
+					onPress={() => dispatch(navigate('EditProfile'))}
+				/>
+			</Section>
+			{(name || email || phoneNumber) && (
+				<Section header={'Overview'}>
+					{name && <KeyValueCell title={'Full Name'} value={name} />}
+					{email && <KeyValueCell title={'Email'} value={email} />}
+					{phoneNumber && <KeyValueCell title={'Phone Number'} value={phoneNumber} />}
+				</Section>
+			)}
 			<Section>
 				<StaticCell title={'Log Out'} onPress={() => dispatch(logOut())} />
 			</Section>
